250b3ab2de441927a9cc948fcdf1c385a6e3684c
Baturay Karaduman <baturaykaraduman@Baturays-MacBook-Pro.local>
2019-08-09 20:35:33 +0200

    swagger updated

--- spec/integration/articles_spec.rb
+++ spec/integration/articles_spec.rb
@@ -5,9 +5,41 @@ describe 'Articles API' do
 
   path '/articles' do
 
+    get 'Retrieves all articles' do
+      tags 'Articles'
+      produces 'application/json'
+
+      response '200', 'title found' do
+        schema type: :object,
+          properties: {
+            data: {
+              type: :array,
+              items: {
+                type: :object,
+                required: %i[id type attributes],
+                properties: {
+                  id: { type: :string },
+                  type: { type: :string },
+                  attributes: {'$ref' => '#/definitions/article'}
+                }
+              }
+            }
+          },
+          required: [ 'id' ]
+
+        let(:id) { Article.create(title: 'foo', description: 'bar bar foobarbar bar foobarbar bar foobarbar bar foobar').id }
+        run_test!
+      end
+
+      response '404', 'article not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+
     post 'Creates a article' do
       tags 'Articles'
-      consumes 'application/json', 'application/xml'
+      consumes 'application/json'
       parameter name: :article, in: :body, schema: {
         '$ref' => '#/definitions/create_article'
       }
@@ -28,26 +60,23 @@ describe 'Articles API' do
 
     get 'Retrieves an article' do
       tags 'Articles'
-      produces 'application/json', 'application/xml'
+      produces 'application/json'
       parameter name: :id, :in => :path, :type => :string
 
       response '200', 'title found' do
         schema type: :object,
           properties: {
             data: {
-              type: :array,
-              items: {
-                type: :object,
-                required: %i[id type attributes],
-                properties: {
-                  id: { type: :string },
-                  type: { type: :string },
-                  attributes: {'$ref' => '#/definitions/article'}
-                }
+              type: :object,
+              required: %i[id type attributes],
+              properties: {
+                id: { type: :string },
+                type: { type: :string },
+                attributes: {'$ref' => '#/definitions/article'}
               }
             }
           },
-          required: [ 'id', 'title', 'description' ]
+          required: [ 'id' ]
 
         let(:id) { Article.create(title: 'foo', description: 'bar bar foobarbar bar foobarbar bar foobarbar bar foobar').id }
         run_test!
@@ -55,6 +84,41 @@ describe 'Articles API' do
 
       response '404', 'article not found' do
         let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+
+    put 'Updates an article' do
+      tags 'Articles'
+      consumes 'application/json'
+      parameter name: :id, :in => :path, :type => :string
+      parameter name: :article, in: :body, schema: {
+        '$ref' => '#/definitions/create_article'
+      }
+
+      response '201', 'article updated' do
+        let(:article) { { title: 'Baturay', description: 'creator of this api, rails magician/creator of this api, rails magician' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:article) { { title: 'foo' } }
+        run_test!
+      end
+    end
+
+    delete 'Delete an article' do
+      tags 'Articles'
+      consumes 'application/json'
+      parameter name: :id, :in => :path, :type => :string
+
+      response '201', 'article deleted' do
+        let(:article) { { title: 'Baturay', description: 'creator of this api, rails magician/creator of this api, rails magician' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:article) { { title: 'foo' } }
         run_test!
       end
     end

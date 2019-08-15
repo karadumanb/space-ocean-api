90d7475d15c900cfe830426d2e89fefbcc40708c
Baturay Karaduman <baturaykaraduman@Baturays-MBP.fritz.box>
2019-08-15 12:37:12 +0200

    admin removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles, dependent: :destroy
     validates :username, uniqueness: { case_sensitive: false }, presence: true, length: {minimum: 3, maximum: 15}
     has_secure_password
 end
